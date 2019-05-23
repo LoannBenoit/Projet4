0b5d5f3208f01750da3f4907b953c84ebefe1c91
loann benoit <loann.benoit@hotmail.com>
2019-05-23 17:42:24 +0200

    Adding materialize to the project

--- templates/home.php
+++ templates/home.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/materialize.min.css">
     <title><?php $title ?></title>
 </head>
 <body>
+    Hello World !
     
+    <script src="../js/materialize.min.js"></script>
 </body>
 </html>

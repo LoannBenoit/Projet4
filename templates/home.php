e695bc5ca40fa1e45fe586d6f5696a54b4c4882b
loann benoit <loann.benoit@hotmail.com>
2019-05-23 19:03:59 +0200

    Creating navbar and adding jQuery

--- templates/home.php
+++ templates/home.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/materialize.min.css">
-    <title><?php $title ?></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/materialize.min.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <title><?php echo $title ?></title>
 </head>
 <body>
-    Hello World !
-    
-    <script src="../js/materialize.min.js"></script>
+<div class="nav-content">
+      <ul class="tabs">
+        <li class="tab"><a href="#Accueil">Accueil</a></li>
+        <li class="tab"><a href="#/chapitres">Derniers Chapitres</a></li>
+        <li class="tab"><a href="#test3">A propos</a></li>
+        <li class="tab"><a href="#test4">Admin</a></li>
+      </ul>
+    </div>
+  
+  
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  <script src="../js/materialize.min.js"></script>
+  <script src="../js/index.js"></script>
 </body>
 </html>

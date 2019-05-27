64a762fc5e2948e209d0090e09f09761b3cc2447
loann benoit <loann.benoit@hotmail.com>
2019-05-28 00:38:26 +0200

    Deleting Jean's photo to the home page

--- templates/home.php
+++ templates/home.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="../css/materialize.min.css">
   <link rel="stylesheet" href="../css/style.css">
-  <title><?php echo $title ?></title>
+  <title>Un billet pour l'Alaska | Accueil</title>
 </head>
 <body>
 
@@ -14,14 +14,15 @@
       <ul class="tabs">
         <li class="tab"><a href="#Accueil">Accueil</a></li>
         <li class="tab"><a href="#/chapitres">Chapitres</a></li>
-        <li class="tab"><a href="#test3">A propos</a></li>
+        <li class="tab"><a href="#test3">à propos</a></li>
         <li class="tab"><a href="#test4">Admin</a></li>
       </ul>
     </div>
-    <div class="photo"><img src="../images/Photo_ronde.png" alt="Photo de Jean"></div>
     <div class="background">
-  <img src="../images/background.jpg" alt="background">
-</div>
+      <img src="../images/background.jpg" alt="background">
+    </div>
+
+    <div class="big-title">Un billet pour l'Alaska</div>
   
   <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
